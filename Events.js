1e8034d3ce35819535c6821073bd8528d093e654
Rawnii <ryanmooney22@gmail.com>
2023-06-15 09:07:37 -0600

    new format
    hopefully easier to import and search

---
+++ Events.js
@@ -0,0 +1,40 @@
+export const EVENTS ={
+        "firstevent": {             //lowercase event name used for search
+            slug: "firstevent",     //lowercase event name used for navigation
+            name: "FirstEvent",
+            body: {
+                imgOne: "https://cdn.pixabay.com/photo/2015/02/13/09/49/pay-634914_960_720.png",
+                ImgTwo: "https://cdn.pixabay.com/photo/2013/07/12/16/22/number-150791_960_720.png",
+                imgThree: "https://cdn.pixabay.com/photo/2015/02/13/09/48/pay-634912_960_720.png",
+                paraOne: `paragraph 1`,
+                paraTwo: `paragraph 2`,
+                paraThree: `paragraph 3`
+            }
+        },
+
+        "secondevent": {            //lowercase event name used for search
+            slug: "secondevent",    //lowercase event name used for navigation
+            name: "SecondEvent",
+            body: {
+                imgOne: "https://cdn.pixabay.com/photo/2015/02/13/09/49/pay-634914_960_720.png",
+                ImgTwo: "https://cdn.pixabay.com/photo/2013/07/12/16/22/number-150791_960_720.png",
+                imgThree: "https://cdn.pixabay.com/photo/2015/02/13/09/48/pay-634912_960_720.png",
+                paraOne: `paragraph 1`,
+                paraTwo: `paragraph 2`,
+                paraThree: `paragraph 3`
+            }
+        },
+
+        "thirdevent": {             //lowercase event name used for search
+            slug: "thirdevent",     //lowercase event name used for navigation
+            name: "ThirdEvent",
+            body: {
+                imgOne: "https://cdn.pixabay.com/photo/2015/02/13/09/49/pay-634914_960_720.png",
+                ImgTwo: "https://cdn.pixabay.com/photo/2013/07/12/16/22/number-150791_960_720.png",
+                imgThree: "https://cdn.pixabay.com/photo/2015/02/13/09/48/pay-634912_960_720.png",
+                paraOne: `paragraph 1`,
+                paraTwo: `paragraph 2`,
+                paraThree: `paragraph 3`
+            }
+        }
+}
