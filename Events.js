56f3069b67c6a0a035930c1fea64862397e8b321
Rawnii <ryanmooney22@gmail.com>
2023-07-28 17:56:57 -0600

    Create Events.js

---
+++ Events.js
@@ -0,0 +1,24 @@
+//this is a static file for format 
+
+[
+    {
+        "Website Is Live": {
+            "name": "Website Is Live",
+            "date": "Sometime in the future",
+            "where": "World Wide Web",
+            "description": "This is a preparatory Event",
+            "imgLink": "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/352684964_1696996610751467_728038966502830033_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=730e14&_nc_ohc=80Z1DMb01gAAX_NKmgI&_nc_ht=scontent-lga3-2.xx&oh=00_AfDbiPhlVq2UgLENQWKOzSLZZX-Tk9VzWnoL64ye98K9kA&oe=6496DD77",
+            "fullInfo": {
+                "imgLink1": "https://previews.dropbox.com/p/thumb/AB8JMd9Fw69u2Ji8-XuKdu5ylz_dIGURFI29Zk7-C30qNn-xx1s6pqilKmKqKnxYfJofq7ksVjqgNdRrH-rLPaQzyzKCM_lmTsd6cOFu3Dx01iUbbnrxANMbl-P02wo3qzgi7MLF3P8E8RGZ5xkDli7Z5P_GT9-OfAoYhpMvAhkzxip88_thsDpjG8dMmVdMqGE17N1FZrRcyqZDfRkOlH51M6sPYWMEcMtEWoAITMFt-83C13OEfiIdFqwhtkUnPmysBKCZk9LX6yw80HzFqe6luFP5G9s49uoMGMDd_QWMSYCEJneFXydgI5Hnm4xeSW-QhmEzn8_B3JwSExxC4lDfeHdOJodDg9KVcwLEssFcLMdmK0cl_nuwJmRV-pFUUTc/p.jpeg",
+                "text1": "Text about img 1",
+                "imgLink2": "https://cdn.pixabay.com/photo/2013/07/12/16/22/number-150791_960_720.png",
+                "text2": "Text about img 2",
+                "imgLink3": "https://cdn.pixabay.com/photo/2015/02/13/09/48/pay-634912_960_720.png",
+                "text3": "Text about img 3"
+            }
+        },
+            "metadata": { 
+            "ID":1
+        }
+    },
+]
